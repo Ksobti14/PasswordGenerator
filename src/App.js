6bf375e6ad6b4b396d2047f8358d259df058ff2e
Kanav Sobti <ksobti_be22@thapar.edu>
2024-06-13 17:27:39 +0530

    PGusingreact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useCallback, useState} from 'react';
 function App() {
+  const [length,setlength]=useState(8);
+  const [number,setnumber]=useState(false);
+  const [char,setchar]=useState(false);
+  const [password,setpassword]=useState("");
+  const passwordgenerator =useCallback(()=>{
+      let pass="";
+      let str="ABCDEFGHIJKLMNOPQRSTWXYZabcdefghijklmnopqrstwxyz";
+      if(number){
+        str+="0123456789";
+      }
+      if(char){
+        str+="!@#$%^&*()}{+-~";
+      }
+      for(let i=0;i<length;i++){
+        let randomno=Math.floor(Math.random()*str.length+1);
+        pass+=str.charAt(randomno);
+      }
+      setpassword(pass);
+  },[length,number,char])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <h1>Password Generator</h1>
+        <div>
+          <label>
+            Length:
+            <input
+              type="number"
+              value={length}
+              onChange={(e) => setlength(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Include Numbers:
+            <input
+              type="checkbox"
+              checked={number}
+              onChange={(e) => setnumber(e.target.checked)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Include Special Characters:
+            <input
+              type="checkbox"
+              checked={char}
+              onChange={(e) => setchar(e.target.checked)}
+            />
+          </label>
+        </div>
+        <button onClick={passwordgenerator}>Generate Password</button>
+        <div>
+          <h3>Your Password:</h3>
+          <p>{password}</p>
+        </div>
+      </div>
     </div>
   );
 }
